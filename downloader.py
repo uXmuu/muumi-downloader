fc3ca0ea1e5f57f744e960c44f74f2ec49d6ae96
uXmuu <78486414+uXmuu@users.noreply.github.com>
2022-01-04 15:41:21 +0000

    Create downloader.py

---
+++ downloader.py
@@ -0,0 +1,20 @@
+import os
+os.system("yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' -f 18  https://youtube.com/playlist\?list\=PLrYFoqKfK4xA2fB_9neISqA26IlykR655")
+os.system("sh deskissä.sh")
+for i in os.popen("ls").read().split("\n"):
+    if "[" in i:
+        a=i[:-18]+".mp4"
+        os.system("mv "+'"'+i+'"' +" "+'"'+a+'"')
+        if "mp4" in i[:-4]:
+            a=i[:-4]
+            os.system("mv "+'"'+i+'"' +" "+'"'+a+'"')
+
+    if "linkki" in i.lower():
+        os.remove(i)
+
+
+
+for i in os.popen("ls").read().split("\n"):
+    a = i.replace("🇫","")
+    a = a.replace("Muumilaakson tarinoita jakso ","")
+    os.system("mv "+'"'+i+'"' +" "+'"'+a+'"')
